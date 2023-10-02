13f8462ab720381c11971e0a998faf70c52904b9
mshuler <shulermj@gmail.com>
2023-10-02 11:49:40 -0700

    test

--- backend/db/models/mech.js
+++ backend/db/models/mech.js
@@ -53,7 +53,7 @@ module.exports = (sequelize, DataTypes) => {
       )
       Mech.hasOne(
         models.Shield,{
-          foreignKey:'right_shoulder'
+          foreignKey:'id'
         }
       ),
       Mech.hasOne(
